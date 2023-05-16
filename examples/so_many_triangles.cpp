6ec144fe0f03bb8277ce606f8243e14c6812c5f9
iwishiwasaneagle <jh.ewers@gmail.com>
2023-05-16 12:30:31 +0100

    feat: More examples

---
+++ examples/so_many_triangles.cpp
@@ -0,0 +1,23 @@
+// #  Copyright 2023 Jan-Hendrik Ewers
+// #  SPDX-License-Identifier: GPL-3.0-only
+
+#include <iostream>
+#include <cubpackpp/cubpackpp.h>
+
+real f(const Point &p) {
+    real x = p.X();
+    return x*x;
+}
+
+int main() {
+    REGION_COLLECTION triangles;
+
+    for(int i=0; i<100; i++){
+        Point A(i, i), B(i, i+1), C(i+1, i);
+        triangles += TRIANGLE(A,B,C);
+    }
+
+    std::cout << "The integral is " << Integrate(f, triangles, 0, 0.5e-4)
+              << " with absolute error " << triangles.AbsoluteError() << std::endl;
+    return 0;
+}
